32c0b3cd34933e1bc51c5988f43b540d549dd67a
prasanta <prasantakakati1994@gmail.com>
2022-12-14 11:39:11 +0530

    modified .dockerignore

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import validateEnv from "@utils/validateEnv";
 import MinerConfigRoute from "@routes/minerconfig.routes";
 import UserIdRoute from "@routes/userid.route";
 
+// TODO: Add sentry
+
 validateEnv();
 
 const app = new App([new UserIdRoute(), new MinerConfigRoute()]);
